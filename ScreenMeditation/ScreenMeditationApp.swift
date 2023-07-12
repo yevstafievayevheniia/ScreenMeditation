be680b06e47741512a22611da4ea625d1126281b
MisJarvis <mizzz.jarvizzz@gmail.com>
2023-07-12 21:12:07 +0300

    Initial Commit

---
+++ ScreenMeditation/ScreenMeditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenMeditationApp.swift
+//  ScreenMeditation
+//
+//  Created by Yevstafieva Yevheniia on 12.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScreenMeditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
